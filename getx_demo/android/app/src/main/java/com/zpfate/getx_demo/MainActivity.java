13318bad30a85c78dffa855257369f7ea550f659
zpfate <783919313@qq.com>
2021-05-24 22:05:16 +0800

    新建flutter分支

--- getx_demo/android/app/src/main/java/com/zpfate/getx_demo/MainActivity.java
+++
@@ -1,6 +0,0 @@
-package com.zpfate.getx_demo;
-
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
-}
--- provider_demo/android/app/src/main/java/com/zpfate/provider_demo/MainActivity.java
+++
@@ -1,6 +0,0 @@
-package com.zpfate.provider_demo;
-
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
-}
