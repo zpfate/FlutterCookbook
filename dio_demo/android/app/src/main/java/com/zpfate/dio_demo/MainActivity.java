984a7b13b0af9905da0475507082e0abec6cc474
ZpFate <783919313@qq.com>
2021-05-24 22:02:24 +0800

    新建dart分支

--- dio_demo/android/app/src/main/java/com/zpfate/dio_demo/MainActivity.java
+++
@@ -1,6 +0,0 @@
-package com.zpfate.dio_demo;
-
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
-}
--- getx_demo/lib/count_controller.dart
+++
@@ -1,8 +0,0 @@
-import 'package:get/get.dart';
-
-class CountController extends GetxController {
-  var count = 0.obs;
-  increment() {
-    count++;
-  }
-}
