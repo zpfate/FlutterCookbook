877845b0a020b13b28a8d28ceb064a489fda76d1
zpfate <783919313@qq.com>
2021-07-21 22:34:58 +0800

    getx demo

--- dart_demo/lib/main.dart
+++ dart_demo/lib/main.dart
@@ -53,6 +53,7 @@ class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
   void _incrementCounter() {
+
     setState(() {
       // This call to setState tells the Flutter framework that something has
       // changed in this State, which causes it to rerun the build method below
