ff9dc671c1037395f2497931c4ab3ddd86abc6a3
zpfate <783919313@qq.com>
2021-07-21 22:35:18 +0800

    dart demo

---
+++ dart_demo/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/twistedfate/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/twistedfate/Desktop/TwistedFate/GitHub/FlutterCookbook/dart_demo"
+export "FLUTTER_TARGET=/Users/twistedfate/Desktop/TwistedFate/GitHub/FlutterCookbook/dart_demo/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_DEFINES=flutter.inspector.structuredErrors%3Dtrue"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=/Users/twistedfate/Desktop/TwistedFate/GitHub/FlutterCookbook/dart_demo/.dart_tool/package_config.json"
